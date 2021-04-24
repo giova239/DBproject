32b813eaec845bd07acd98286e45d0a9292caba3
giova239 <stevanay@gmail.com>
2021-04-24 17:36:10 +0200

    implemented dynamic update of createSurvey page to add more questions

--- static/materialize/js/init.js
+++ static/materialize/js/init.js
@@ -3,7 +3,7 @@
     $(document).ready(function(){
         $('.sidenav').sidenav();
         $('input#input_text, textarea#textarea2').characterCounter();
-        $( ".datepicker" ).datepicker({
+        $( '.datepicker' ).datepicker({
             format: 'yyyy-mm-dd',
             yearRange: [1900,2050],
             changeMonth: true,
@@ -11,6 +11,24 @@
         });
         $('.timepicker').timepicker();
         $('select').formSelect();
+        var i = 2;
+        $('#addQuestion').click (function(){
+            var qForm = $('#questionForm1').clone();
+            qForm.attr('id','questionForm'+i);
+            qForm.find('h6.questionTitle').html('Question #'+i);
+            qForm.find('a.questionCloseButton').attr('id','questionCloseButton'+i);
+            qForm.find('a.questionCloseButton').on('click', function(){alert("test")});
+            qForm.find('input.questionText').attr('name', 'question_text_'+i);
+            qForm.find('input.questionText').attr('id', 'icon_question_text_'+i);
+            qForm.find('label.questionTextLabel').attr('for', 'icon_question_text_'+i);
+            $.get("static/html/select.html").success(function(data) {
+                qForm.find('div.questionTypeSelectWrapper').html(data)
+            });
+            qForm.find('select.questionType').attr('id','question_type_'+i);
+            $('#addHere').append(qForm);
+            i++;
+            setTimeout(function(){qForm.find('select.questionType').formSelect();},10);
+        });
     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
